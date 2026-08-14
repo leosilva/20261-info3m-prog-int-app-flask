7b1a412f6be0d98a1d59a6ebf6af655141c556b8
Leo Silva <leo.moreira@me.com>
2026-08-14 10:30:45 -0300

    exibindo formulários na web e recebendo os dados

---
+++ app/forms/login_form.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import BooleanField, PasswordField, StringField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Usuário', validators=[DataRequired()])
+    password = PasswordField('Senha', validators=[DataRequired()])
+    remember_me = BooleanField('Permanecer Conectado')
+    submit = SubmitField('Entrar')
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from app import app
 from flask import render_template
 
+from app.forms.login_form import LoginForm
+
 
 @app.route('/')
 def home():
@@ -20,3 +22,14 @@ def sobre():
 @app.route('/endereco')
 def endereco():
     return "<h1>Meu endereço</h1>"
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    formulario = LoginForm()
+    if formulario.validate_on_submit():
+        print(formulario.username.data)
+        print(formulario.password.data)
+        return render_template("index.html")
+    return render_template('login.html', 
+                           title='Login', 
+                           form=formulario)
