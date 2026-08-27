60eba500ed6195163dcbc2991c9b79086b8eedb0
Leo Silva <leo.moreira@me.com>
2026-08-27 10:12:01 -0300

    adicao de links no header; mensagens personalizadas de validacao; validadores personalizados.

--- app/forms/login_form.py
+++ app/forms/login_form.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Usuário', validators=[DataRequired()])
-    password = PasswordField('Senha', validators=[DataRequired()])
+    username = StringField('Usuário', validators=[DataRequired(message="Por favor, preencha o seu nome")])
+    password = PasswordField('Senha', validators=[DataRequired(message="Por favor, preencha a senha")])
     remember_me = BooleanField('Permanecer Conectado')
     submit = SubmitField('Entrar')
--- app/forms/usuario_form.py
+++ app/forms/usuario_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, PasswordField, StringField, SubmitField, EmailField
-from wtforms.validators import DataRequired, EqualTo
+from wtforms.validators import DataRequired, EqualTo, ValidationError
 
 
 class UsuarioForm(FlaskForm):
@@ -10,3 +10,7 @@ class UsuarioForm(FlaskForm):
     confirmacao_senha = PasswordField('Confirmação de Senha', 
                                       validators=[DataRequired(), EqualTo('senha')])
     submit = SubmitField('Entrar')
+    
+    def validate_nome_completo(self, field):
+        if field.data.lower() == 'admin':
+            raise ValidationError('O nome "admin" está reservado. Escolha outro.')
